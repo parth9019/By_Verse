e7ccbca8dfca5e1deb08ac0aa051bb4fae30821a
parth9019 <patelparth9019@gmail.com>
2026-04-28 19:41:45 +0530

    fixed base url with api prefix

--- Ecom/src/api/axios.js
+++ Ecom/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://by-verse.onrender.com",
+  baseURL: "https://by-verse.onrender.com/api",
 });
 
 api.interceptors.request.use(
